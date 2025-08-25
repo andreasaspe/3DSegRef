78348f9c6e406b14c4ae538774f03eb2c1005fd7
Andreas With Aspe <awias@dtu.dk>
2025-08-25 16:59:55 +0200

    Dice score script and some predicting script? Maybe one of them are wrong..?

---
+++ nnunet/TotalSegmentator/Calculate_dice_scores.py
@@ -0,0 +1,59 @@
+import os
+from tqdm import tqdm
+import numpy as np
+import SimpleITK as sitk
+
+def dice_score(y_true, y_pred, smooth=1e-6):
+    """
+    Compute the Dice score between two binary masks.
+
+    Args:
+        y_true (numpy.ndarray): Ground truth binary mask.
+        y_pred (numpy.ndarray): Predicted binary mask.
+        smooth (float): Smoothing factor to avoid division by zero.
+
+    Returns:
+        float: Dice score.
+    """
+    intersection = np.sum(y_true * y_pred)
+    return (2. * intersection + smooth) / (np.sum(y_true) + np.sum(y_pred) + smooth)
+
+label_gt_path = "/scratch/awias/data/nnUNet/nnUNet_raw/Dataset004_TotalSegmentatorPancreas/labelsTs"
+label_pred_path = "/scratch/awias/data/nnUNet/nnUNet_raw/Dataset004_TotalSegmentatorPancreas/imagesTs/man_preds"
+
+label_gt_file = [x for x in os.listdir(label_gt_path) if x.endswith(".nii.gz")]
+label_pred_file = [x for x in os.listdir(label_pred_path) if x.endswith(".nii.gz")]
+
+all_subjects = [x.split('_')[0] for x in label_pred_file]
+
+#Make sure the files are sorted in the same order
+label_gt_file = sorted(label_gt_file, key=lambda x: x.split('_')[0])
+label_pred_file = sorted(label_pred_file, key=lambda x: x.split('_')[1])
+
+# if len(label_gt_file) != len(label_pred_file):
+#     raise ValueError("Number of ground truth and prediction files do not match")
+
+dice_list = []
+n = len(label_gt_file)
+
+for subject in all_subjects:
+    try:
+        mask_gt = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join(label_gt_path, f"{subject}.nii.gz")))
+        pred = sitk.GetArrayFromImage(sitk.ReadImage(os.path.join(label_pred_path, f"{subject}_0000_pred.nii.gz")))
+        
+        # print(os.path.join(label_gt_path,label_gt_file[i]))
+        # print(os.path.join(label_pred_path,label_pred_file[i]))
+        # tools.plot_central_slice_img_mask(mask_gt, pred, spacing=None)
+        
+
+        dice = dice_score(mask_gt, pred)
+        dice_list.append(dice)
+        
+        print(f"Dice score for subject {subject} is: {dice}")
+    except:
+        print(f"Error processing subject {subject}. Skipping...")
+
+print(f"Mean dice score: {sum(dice_list) / len(dice_list)}")
+print(f"Median dice score: {sorted(dice_list)[len(dice_list) // 2]}")
+print(f"Max dice score: {max(dice_list)}")
+print(f"Min dice score: {min(dice_list)}")
---
+++ nnunet/Verse/predict_with_nnUNet.py
@@ -0,0 +1,75 @@
+import argparse
+from nnunetv2.paths import nnUNet_results, nnUNet_raw
+import torch
+from batchgenerators.utilities.file_and_folder_operations import join
+from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
+import json
+import os
+import SimpleITK as sitk
+import numpy as np
+from skimage.measure import label
+from pathlib import Path
+import os
+
+def predict_with_nn_unet_on_filelist():
+    print("Predict with NN U-Net")
+    model_folder = "/scratch/awias/data/nnUNet_dataset/nnUNet_results/Dataset004_Panc/nnUNetTrainerNoMirroring__nnUNetResEncUNetLPlans__3d_fullres"
+
+    input_data_folder = "/scratch/awias/data/Pancreas/nnUNet_dataset/nnUNet_raw/Dataset001_Pancreas/imagesTs"
+    output_folder = "/scratch/awias/data/Pancreas/nnUNet_dataset/nnUNet_raw/Dataset001_Pancreas/imagesTs/man_preds"
+
+    os.environ["nnUNet_results"] = "/scratch/awias/data/Pancreas/nnUNet_dataset/nnUNet_results"
+    
+    os.makedirs(output_folder, exist_ok=True)
+
+    in_files = []
+    out_files = []
+    
+    for file in os.listdir(input_data_folder):
+        if file.endswith("nii.gz"):
+            subject = file.split(".nii.gz")[0]
+            in_files.append([os.path.join(input_data_folder, file)])
+            out_files.append(os.path.join(output_folder, subject + "_pred.nii.gz"))
+        
+    print(f"Initializing class")
+    # instantiate the nnUNetPredictor
+    predictor = nnUNetPredictor(
+        tile_step_size=0.5,
+        use_gaussian=True,
+        use_mirroring=False,
+        perform_everything_on_device=True,
+        device=torch.device('cuda', 0),
+        verbose=False,
+        verbose_preprocessing=False,
+        allow_tqdm=True
+    )
+
+    print(f"Initializing from trained model folder")
+
+    # initializes the network architecture, loads the checkpoint
+    predictor.initialize_from_trained_model_folder(
+        model_folder,
+        use_folds=[0],
+        checkpoint_name='checkpoint_final.pth',
+    )
+
+    print(f"Predicting from files")
+
+    predictor.predict_from_files(in_files,
+                                     out_files,
+                                     save_probabilities=False, overwrite=True,
+                                     num_processes_preprocessing=1, num_processes_segmentation_export=1,
+                                     folder_with_segs_from_prev_stage=None, num_parts=1, part_id=0)
+
+
+
+
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='dtu-predict-with-nn-unet')
+    # config = DTUConfig(args)
+    # if config.settings is not None:
+    #     # predict_with_nn_unet(config)
+
+    predict_with_nn_unet_on_filelist()
